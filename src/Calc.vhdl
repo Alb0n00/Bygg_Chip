f1721a859461fc23313e08c6894c3de9d5bd11d9
albha152 <albha152@muxen2-113.ad.liu.se>
2025-04-05 14:13:47 +0200

    Change width of numb_a and b

--- src/Calc.vhdl
+++ src/Calc.vhdl
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity tt_um_big_calc is
+entity tt_um_example is
     port (
         ui_in   : in  std_logic_vector(7 downto 0);
         uo_out  : out std_logic_vector(7 downto 0);
@@ -13,13 +13,14 @@ entity tt_um_big_calc is
         clk     : in  std_logic;
         rst_n   : in  std_logic
     );
-end tt_um_big_calc;
+end tt_um_example;
 
-architecture Behavioral of tt_um_big_calc is
-signal numb_a : unsigned(3 downto 0);
-signal numb_b : unsigned(3 downto 0);
+architecture Behavioral of tt_um_example is
+signal numb_a : unsigned(7 downto 0);
+signal numb_b : unsigned(7 downto 0);
 signal c : unsigned(7 downto 0);
 signal op :  unsigned(1 downto 0);
+-- en till signal för opp
 begin
     numb_a <= unsigned(ui_in(3 downto 0));
     numb_b <= unsigned(ui_in(7 downto 4));
